ab0f81ecc5be69d9f526ecaac1c6426e88ed8552
developernajib <developernajib@gmail.com>
2024-11-14 19:11:51 +0600

    demo function

--- demo/functions/functions.go
+++ demo/functions/functions.go
@@ -2,7 +2,17 @@ package main
 
 import "fmt"
 
-func main() {
-
+func add(number1, number2 int) int {
+	return number1 + number2
 }
 
+func greet(name string) string {
+	return "Hello, " + name
+}
+
+func main() {
+	result := add(2, 9)
+	fmt.Println("The sum is", result)
+
+	fmt.Println(greet("John Doe"))
+}
