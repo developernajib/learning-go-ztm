8710ef66cba979f2db723ecd07a47eab220243cd
developernajib <developernajib@gmail.com>
2024-11-20 09:45:59 +0600

    loops

--- demo/loops/loops.go
+++ demo/loops/loops.go
@@ -3,6 +3,15 @@ package main
 import "fmt"
 
 func main() {
+	sum := 0
+	// fmt.Println("Sum is", sum)
+	for i := 0; i <= 100; i++ {
+		sum += i
+		fmt.Println("Sum is", sum)
+	}
 
+	for sum >= 500 {
+		sum -= 10
+		fmt.Println("Decrement Sum is", sum)
+	}
 }
-
