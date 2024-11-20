1d81e7c4fa38eefe234d5a3ca059face40b2d912
developernajib <developernajib@gmail.com>
2024-11-20 09:35:34 +0600

    switch

--- exercise/switch/switch.go
+++ exercise/switch/switch.go
@@ -14,5 +14,17 @@ package main
 import "fmt"
 
 func main() {
-}
 
+	switch age := 15; {
+	case age == 0:
+		fmt.Println("newborn")
+	case age >= 1 && age <= 3:
+		fmt.Println("toddler")
+	case age >= 4 && age <= 12:
+		fmt.Println("child")
+	case age >= 13 && age <= 17:
+		fmt.Println("teenager")
+	case age >= 18:
+		fmt.Println("adult")
+	}
+}
