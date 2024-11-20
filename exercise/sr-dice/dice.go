74e63119700470e4a3ac39b1e8e193d4f243ce89
developernajib <developernajib@gmail.com>
2024-11-20 10:14:42 +0600

    sr-dice exercise

--- exercise/sr-dice/dice.go
+++ exercise/sr-dice/dice.go
@@ -23,8 +23,60 @@
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+)
 
-func main() {
+func rollDice(sides int) int {
+	return rand.Intn(sides) + 1
 }
 
+func main() {
+	// numRolls := 10
+	// numDice := 2
+	// numSides := 6
+
+	// for totalRoll := 0; totalRoll < numRolls; totalRoll++ {
+	// 	rollSum := 0
+	// 	for totalDice := 0; totalDice < numDice; totalDice++ {
+	// 		rollSum += rand.Intn(numSides) + 1
+	// 	}
+
+	// 	fmt.Printf("Roll #%d: %d\n", totalRoll+1, rollSum)
+
+	// 	if rollSum == 2 && numDice == 2 {
+	// 		fmt.Println("Snake eyes")
+	// 	} else if rollSum == 7 {
+	// 		fmt.Println("Lucky 7")
+	// 	} else if rollSum%2 == 0 {
+	// 		fmt.Println("Even")
+	// 	} else {
+	// 		fmt.Println("Odd")
+	// 	}
+	// }
+
+	dice, sides := 2, 6
+	rolls := 3
+
+	for totalRolls := 0; totalRolls < rolls; totalRolls++ {
+		rollSum := 0
+		for totalDice := 0; totalDice < dice; totalDice++ {
+			rolled := rollDice(sides)
+			rollSum += rolled
+			fmt.Printf("Roll #%d: %d\n", totalRolls+1, rolled)
+		}
+		fmt.Println("Total rolled:", rollSum)
+
+		switch sum := rollSum; {
+		case sum == 2 && dice == 2:
+			fmt.Println("Snake eyes")
+		case sum == 7:
+			fmt.Println("Lucky 7")
+		case sum%2 == 0:
+			fmt.Println("Even")
+		case sum%2 == 1:
+			fmt.Println("Odd")
+		}
+	}
+}
