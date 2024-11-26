a14e754cb3a07b755a463ac8f943c5a5a7402540
developernajib <developernajib@gmail.com>
2024-11-26 16:09:38 +0600

    exercise structs

--- exercise/structs/structs.go
+++ exercise/structs/structs.go
@@ -19,7 +19,43 @@ package main
 
 import "fmt"
 
-func main() {
-
+type Coordinate struct {
+	x, y int
 }
 
+type Rectangle struct {
+	a Coordinate
+	b Coordinate
+}
+
+func width(rect Rectangle) int {
+	return (rect.b.x - rect.a.x)
+}
+
+func length(rect Rectangle) int {
+	return (rect.a.y - rect.b.y)
+}
+
+func area(rect Rectangle) int {
+	return width(rect) * length(rect)
+}
+
+func perimeter(rect Rectangle) int {
+	return (width(rect) * 2) + (length(rect) * 2)
+}
+
+func printInfo(rect Rectangle) {
+	fmt.Println("Area is", area(rect))
+	fmt.Println("Perimeter is", perimeter(rect))
+}
+
+func main() {
+	rect := Rectangle{a: Coordinate{0, 7}, b: Coordinate{10, 0}}
+
+	printInfo(rect)
+
+	rect.a.x *= 2
+	rect.a.y *= 2
+
+	printInfo(rect)
+}
