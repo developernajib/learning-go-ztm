ded84636a302314e65a6f4b625c49bb3aa970bf3
developernajib <developernajib@gmail.com>
2025-01-21 12:46:21 +0600

    ranges

--- demo/ranges/ranges.go
+++ demo/ranges/ranges.go
@@ -3,5 +3,18 @@ package main
 import "fmt"
 
 func main() {
-}
+	slice := []string{"Hello", "World", "!"}
 
+	// for _, element := range slice {
+	// 	fmt.Println(element)
+	// }
+
+	for i, element := range slice {
+		fmt.Println(i, element, ":")
+
+		for _, char := range element {
+			fmt.Printf(" %q", char)
+		}
+		fmt.Printf("\n\n")
+	}
+}
