ac875f46f96c0021d56f17d3664f722f71ebc518
developernajib <developernajib@gmail.com>
2025-08-21 08:36:15 +0600

    receiver function with Dario

--- demo/rcv-func/rcv-func.go
+++ demo/rcv-func/rcv-func.go
@@ -2,7 +2,33 @@ package main
 
 import "fmt"
 
-func main() {
-
+type Space struct {
+	occupied bool
 }
 
+type ParkingLot struct {
+	spaces []Space
+}
+
+func occupySpace(spaceNum int, lot *ParkingLot) {
+	lot.spaces[spaceNum-1].occupied = true
+}
+
+func (lot *ParkingLot) occupySpace(spaceNum int) {
+	lot.spaces[spaceNum-1].occupied = true
+}
+
+func (lot *ParkingLot) vacateSpace(spaceNum int) {
+	lot.spaces[spaceNum-1].occupied = false
+}
+
+func main() {
+	lot := ParkingLot{spaces: make([]Space, 5)}
+	fmt.Println("initial:", lot)
+	lot.occupySpace(1)
+	occupySpace(2, &lot)
+	fmt.Println("After occupied:", lot)
+
+	lot.vacateSpace(2)
+	fmt.Println("After vacate:", lot)
+}
