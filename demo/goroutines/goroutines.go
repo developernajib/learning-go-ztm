410dfbde4c9d483c0e2cdf75bcddda35775df109
developernajib <developernajib@gmail.com>
2025-08-24 15:12:53 +0600

    go routine

--- demo/goroutines/goroutines.go
+++ demo/goroutines/goroutines.go
@@ -1,10 +1,27 @@
 package main
 
-import "fmt"
-import "time"
-import "unicode"
+import (
+	"fmt"
+	"time"
+	"unicode"
+)
 
 func main() {
 	data := []rune{'a', 'b', 'c', 'd'}
+	var capitalized []rune
+
+	capIt := func(r rune) {
+		capitalized = append(capitalized, unicode.ToUpper(r))
+		fmt.Printf("%c done!\n", r)
+	}
+
+	for i := 0; i < len(data); i++ {
+		element := data[i]
+		go capIt(element)
+	}
+	time.Sleep(100 * time.Millisecond)
+	fmt.Printf("Capitalized: %c\n", capitalized)
 }
 
+// for i in {1..10}; do go run . | grep ':'; done
+// for i in {1..10}; do go run . | rg ':'; done
